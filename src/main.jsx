cbe523ec8e4e968c48a7c21755cb35b3f20c4950
osparking <jbpark03@gmail.com>
2024-11-30 13:04:24 +0900

    BasicExample compo' used in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import BasicExample from './components/BasicExample'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BasicExample />
   </StrictMode>,
 )
